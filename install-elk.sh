a12e941902f1c6d1efaf82d14ce97351742a6736
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2015-04-16 18:35:11 +0200

    Ajout d'un script d'install pour debian

---
+++ install-elk.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo -e "Installation de Open-JDK7\n"
+apt-get -y install openjdk-7-jre
+echo -e "Installation de Elasticsearch\n"
+echo -e "Création du source list pour Elasticsearch\n"
+wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
+echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' | tee /etc/apt/sources.list.d/elasticsearch.list
+apt-get update
+apt-get -y install elasticsearch=1.4.4
+echo -e "Configuration de Elasticsearch\n"
+echo 'network.host=localhost' >> /etc/elasticsearch/elasticsearch.yml
+echo -e "Demarrage du serveur et ajout du service au demarrage\n"
+service elasticsearch restart
+update-rc.d add elasticsearch defaults 95 10
+echo -e "Elasticsearch pret\n"
+sleep 1
+echo -e "Installation de Kibana\n"
+cd 
+echo -e "Recupérationdes fichiers d'installation\n"
+wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz
+echo -e "Decompression\n"
+tar xvf kibana-*.tar.gz
+echo -e "Configuration\n"
+sed -i -e "s/\"0\.0\.0\.0\"/\"localhost\"/g" ~/kibana-4*/config/kibana.yml
+mkdir -p /opt/kibana
+cp -R ~/kibana-4*/* /opt/kibana/
+echo -e "Ajout de Kibana en tant que service\n"
+cd /etc/init.d && wget https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4
+chmod +x /etc/init.d/kibana4
+update-rc.d add kibana4 defaults 96 9
+service kibana4 start
+echo -e "Kibana pret\n"
+echo -e "Installation du serveur web Nginx\n"
+apt-get -y install nginx apache2-utils
+echo -e "Creation d'un mot de passe pour l'administrateur kibanaadmin\n"
+htpasswd -c /etc/nginx/htpasswd.users kibanaadmin
+echo -e "Configuration du serveur Nginx\n"
+echo "server {
+    listen 80;
+
+    server_name example.com;
+
+    auth_basic "Restricted Access";
+        auth_basic_user_file /etc/nginx/htpasswd.users;
+	
+	    location / {
+            proxy_pass http://localhost:5601;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection 'upgrade';
+	            proxy_set_header Host $host;
+		            proxy_cache_bypass $http_upgrade;        
+			        }
+			}" > /etc/nginx/sites-available/default
+service nginx restart
+echo -e "Nginx pret\n"
+echo -e "Installation de Logstash\n"
+echo 'deb http://packages.elasticsearch.org/logstash/1.5/debian stable main' | tee /etc/apt/sources.list.d/logstash.list
+apt-get update
+apt-get -y install logstash
+echo -e "Configuration de Logstash\n"
+cat logstash.conf > /etc/logstash/conf.d/logstash.conf
+service logstash restart
+echo -e "Logstash pret\n"
+echo -e "Pile ELK prete !!\n"
